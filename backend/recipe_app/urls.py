9fa1659cd07fd4db20da5b11ab50511f24ce5cf1
Elia Partin <arenaselia@gmail.com>
2023-07-11 13:56:46 -0400

    fixed crud function in restful api and added detailview

--- backend/recipe_app/urls.py
+++ backend/recipe_app/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     path("home/", views.HomeView.as_view(), name="home"),
-    # path('', views.getFood),
-    # path('post/', views.postFood),
+    # path("", views.getRecipe),
+    # path("post/", views.postRecipe),
+    path("recipes/", views.RecipeListAPIView.as_view(), name="recipe_list"),
+    path("recipes/<int:pk>/", views.RecipeDetailView.as_view(), name="recipe_detail"),
+    # path("update/", views.updateRecipe),
+    # path("delete/", views.deleteRecipe),
     # path("logout/", views.LogoutView.as_view(), name="logout"),
 ]
--- backend/recipe_app/views.py
+++ backend/recipe_app/views.py
@@ -7,16 +7,58 @@ from django.contrib.auth import authenticate, login
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.decorators import api_view
+from .models import Recipe
+from .serializer import RecipeSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
 # Create your views here.
 class HomeView(APIView):
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request):
-        content = {'message': 'Welcome to the Aunthentication page!'}
+        content = {"message": "Welcome to the Aunthentication page!"}
         return Response(content)
 
-  
-# @api_view(['GET'])
-# def getFood(request):
-#     return Response()
 
-          
+class RecipeListAPIView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
+    
+class RecipeDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
+
+
+# @api_view(['GET'])
+# def getRecipe(request):
+#     recipe = Recipe.objects.all()
+#     serializer = RecipeSerializer(recipe, many=True)
+#     return Response(serializer.data)
+
+# @api_view(['POST'])
+# def postRecipe(request):
+#     serializer = RecipeSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+
+# @api_view(['PUT'])
+# def updateRecipe(request):
+#     serializer = RecipeSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+
+# @api_view(['DELETE'])
+# def deleteRecipe(request):
+#     serializer = RecipeSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+
+
+#  https://www.makeuseof.com/django-rest-api-create/
